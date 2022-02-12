18fa2aa3e12b249dd4b24c0fc4e1e053436d6955
SalmaSafe <salmaasulee@gmail.com>
2022-02-12 12:41:07 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+fun main() {
+    var name = "Salma"
+    println(name)
+
+    var age = "20"
+    println(age)
+
+    var phoneNumber = "254790030046L"
+    println(phoneNumber)
+
+    var weightInKg = "46"
+    println(weightInKg)
+
+    var areYouKenyan : Boolean = true
+    println(areYouKenyan)
+
+
+}
